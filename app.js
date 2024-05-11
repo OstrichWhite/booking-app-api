b785f31b59072c2053de43d257acd12b6532c098
OstrichWhite <rahimcsestudies@gmail.com>
2024-05-11 21:53:07 +0530

    Introduced POST register.

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require("express");
+const cors = require("cors");
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:5173",
+  })
+);
+app.use(express.json());
+
 app.get("/info", (req, res) => {
   res.json({ message: "success" });
+});
+
+app.post("/register", (req, res) => {
+  res.json(req.body);
 });
 
 app.listen(PORT, () => console.log("server listen on port " + PORT));

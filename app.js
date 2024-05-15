a213e68d3eef2d5a1f2a7aa4b3df1905b921de53
OstrichWhite <rahimcsestudies@gmail.com>
2024-05-15 21:53:53 +0530

    Registered user saved on DB.

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const { PORT = 3000 } = process.env;
+const bcrypt = require("bcryptjs");
+const User = require("./models/User");
+const { PORT = 3000, PASSWORD_SALT, MONGO_URL } = process.env;
 
 const app = express();
 
@@ -12,16 +14,24 @@ app.use(
     origin: "http://localhost:5173",
   })
 );
+
 app.use(express.json());
-mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => console.log("DB connected."));
+
+mongoose.connect(MONGO_URL).then(() => console.log("DB connected."));
+
 app.get("/info", (req, res) => {
   res.json({ message: "success" });
 });
 
-app.post("/register", (req, res) => {
-  res.json(req.body);
+app.post("/register", async (req, res) => {
+  try {
+    const { name, email } = req.body;
+    const password = bcrypt.hashSync(password, PASSWORD_SALT);
+    const createdUser = await User.create({ name, email, password });
+    res.json(createdUser);
+  } catch (e) {
+    res.status(422).json({ e });
+  }
 });
 
 app.listen(PORT, () => console.log("server listen on port " + PORT));

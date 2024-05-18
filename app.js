75ec70fa990191f02aad2b5929fbe3d3f4c72d08
OstrichWhite <rahimcsestudies@gmail.com>
2024-05-19 04:16:38 +0530

    Introduced POST /login.

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const User = require("./models/User");
-const { PORT = 3000, PASSWORD_SALT, MONGO_URL } = process.env;
+const jwt = require("jsonwebtoken");
+const { PORT = 3000, JWT_SECRET, MONGO_URL } = process.env;
 
 const app = express();
 
@@ -20,7 +21,22 @@ app.use(express.json());
 app.get("/info", (req, res) => {
   res.json({ message: "success" });
 });
-
+const verify = ({ req, res, user }) => {
+  const passOk = bcrypt.compareSync(req.body.password, user.password);
+  if (passOk) {
+    jwt.sign({ id: user._id }, JWT_SECRET, {}, (err, token) => {
+      res.cookie("token", token).json({ message: "Login success." });
+    });
+  } else {
+    res.status(422).json({ message: "Email or password is wrong." });
+  }
+};
+const notFound = (res) => res.status(404).json({ message: "notFound" });
+app.post("/login", async (req, res) => {
+  const { email } = req.body;
+  const user = await User.findOne({ email });
+  user ? verify({ req, res, user }) : notFound(res);
+});
 app.post("/register", async (req, res) => {
   try {
     const { name, email } = req.body;
